53401a29de1922d5b83c263d99ec0c67977d10c1
unknown <erikmaquera@gmail.com>
2022-04-10 10:24:13 -0400

    interface cliente

---
+++ src/app/models/cliente.interface.ts
@@ -0,0 +1,10 @@
+export class Cliente {
+    id?:number;
+    nombre:string;
+    paterno:string;
+    materno:string;
+    genero:number;
+    fecha_nac:any;
+    tipo_doc:number;
+    doc_identidad:string;
+}
